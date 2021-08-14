4f1700b4f17dc57eecb1a3ebf580ca1d3c340dd3
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-08-14 15:54:05 +0500

    Find the non repeating element in array where every element repeating twice

--- src/com/sharjeelmk/Main.java
+++ src/com/sharjeelmk/Main.java
@@ -4,7 +4,7 @@ import com.sharjeelmk.bitManipulation.Xor;
 public class Main {
 
     public static void main(String[] args) {
-    int [] arr = {1,1,33,2,33,44,11,44,11};
-        System.out.println(Xor.nonRepeatingElement1(arr));
+    int [] arr = {1,1,33,2,10,33,44,11,44,11};
+    Xor.nonRepeatingElement2(arr);
     }
 }
--- src/com/sharjeelmk/bitManipulation/Xor.java
+++ src/com/sharjeelmk/bitManipulation/Xor.java
@@ -10,4 +10,26 @@ public class Xor {
         }
     return res;
     }
+    public static void nonRepeatingElement2(int [] arr){
+        int res=0;
+        //Taking xor of all
+        for (int j : arr) {
+            res = res ^ j;
+        }
+        //taking right most set bit for segregation
+        int rmsb= res & -res;
+        // X will responding for rmsb unset
+        // Y will responding for rmsb set
+        int x=0;
+        int y=0;
+        for (int i : arr) {
+            if ((i & rmsb) == 0) {
+                x = x ^ i;
+            } else {
+                y = y ^ i;
+            }
+        }
+        System.out.println(x);
+        System.out.println(y);
+    }
 }
