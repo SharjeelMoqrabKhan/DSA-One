575f9c0d4744846376d5ec0322b2a9a309638ab8
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-08-24 01:37:36 +0500

    buble sort

--- src/com/sharjeelmk/Main.java
+++ src/com/sharjeelmk/Main.java
@@ -1,11 +1,13 @@
 package com.sharjeelmk;
 
-import com.sharjeelmk.array.searching.ArraySearching;
+import com.sharjeelmk.array.sorting.Sorting;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] arr = {11,44,100,404};
-        int key=100;
-        System.out.println(ArraySearching.binarySearch(arr,key));
+        int [] arr = {11,44,100,404,1,14};
+        System.out.println(Arrays.toString(Sorting.bubbleSort(arr)));
+
     }
 }
